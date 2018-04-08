6273668eab47b155fe713990f1dbc403e622ab91
Fabiano Nalin <fnalin@msn.com>
2018-04-08 13:41:47 -0300

    WithMachineName

---
+++ source/03 - DemoSeriLog.WithMachineName/Program.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using System;
+using System.Globalization;
+
+namespace DemoSeriLog.WithMachineName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithMachineName()
+                .Enrich.WithEnvironmentUserName()
+                .WriteTo.Console(
+                        formatProvider: new CultureInfo("pt-BR"),
+                        outputTemplate: "{Timestamp:HH:mm:ss} [{Level}] (Blablablá - ServerName: {MachineName} - UserName: {EnvironmentUserName})"
+                        //github.com/serilog/serilog/wiki/Formatting-Output
+                )
+                .CreateLogger();
+
+
+            Log.Information("Hello again, Serilog!");
+
+            Console.ReadLine();
+        }
+    }
+}
