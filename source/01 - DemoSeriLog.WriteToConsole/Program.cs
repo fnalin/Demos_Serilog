37f0a1d621be118bbc9073273b059f69645a601b
Fabiano Nalin <fnalin@msn.com>
2018-04-08 12:03:52 -0300

    skin - console

---
+++ source/01 - DemoSeriLog.WriteToConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+using System;
+
+namespace DemoSeriLog.WriteToConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando com o new LoggerConfiguration()");
+            using (var log = new LoggerConfiguration()
+                .MinimumLevel.Warning() //De warning p/ cima
+                .WriteTo.Console()
+                .CreateLogger())
+            {
+                log.Information("Hello, Serilog!");
+                log.Warning("Goodbye, Serilog.");
+            }
+
+            Console.WriteLine("Testando com o Static");
+            TesteComOStatic();
+
+            Console.ReadLine();
+        }
+
+        private static void TesteComOStatic()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug() //De Debug p/ cima
+                                      //.MinimumLevel.Information() //De information p/ cima
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Debug("Debug");
+            Log.Information("Hello again, Serilog!");
+            var user = new { Name = "Nick", Id = "nblumhardt" };
+            Log.Information("Logged on user {@User}", user);
+
+            Log.CloseAndFlush();
+        }
+    }
+}
