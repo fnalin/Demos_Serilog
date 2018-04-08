b45b8a411675fbf69de1dd8a769a647b30dcab60
Fabiano Nalin <fnalin@msn.com>
2018-04-08 12:36:54 -0300

    WriteToJSONFile

---
+++ source/02 - DemoSeriLog.WriteToJSONFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using Serilog.Formatting.Compact;
+using System;
+
+namespace DemoSeriLog.WriteToJSONFile
+{
+    //github.com/datalust/clef-tool -> clef
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(new CompactJsonFormatter(), "log.clef")
+                .CreateLogger();
+
+            var itemCount = 99;
+            for (var itemNumber = 0; itemNumber < itemCount; ++itemNumber)
+                Log.Debug("Processing item {ItemNumber} of {ItemCount}", itemNumber, itemCount);
+
+            Log.CloseAndFlush();
+
+            Console.WriteLine("FIM");
+            Console.ReadLine();
+        }
+    }
+}
