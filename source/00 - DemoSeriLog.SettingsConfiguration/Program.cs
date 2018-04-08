37bcb4af29fe675ae021cb1323650b6f9bb108b3
Fabiano Nalin <fnalin@msn.com>
2018-04-08 13:12:49 -0300

    AppSettingsConfiguration

---
+++ source/00 - DemoSeriLog.SettingsConfiguration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace DemoSeriLog.SettingsConfiguration
+{
+    //P/ AppSettings no Console:
+    //docs.microsoft.com/pt-br/aspnet/core/fundamentals/configuration/index?tabs=basicconfiguration
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            logger.Information("Hello, world!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
